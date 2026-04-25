459118019327a08edd5e9fcc70f9d2209543c5fe
YesithTK <yesithszc@gmail.com>
2026-04-24 20:14:19 -0500

    Add SendCommand class extending SendCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/SendCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class SendCommand extends SendCmd {
+    public SendCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
