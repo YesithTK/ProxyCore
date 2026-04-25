98de06d6f674c43ef3a10cf96fb2750b098afb9c
YesithTK <yesithszc@gmail.com>
2026-04-24 20:11:57 -0500

    Add PListCommand class for handling PList commands

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/PListCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class PListCommand extends PListCmd {
+    public PListCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
