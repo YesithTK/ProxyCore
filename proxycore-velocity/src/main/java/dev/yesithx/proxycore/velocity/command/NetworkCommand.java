a0759b380438ceacddc596fe937a9d315e79c642
YesithTK <yesithszc@gmail.com>
2026-04-24 20:10:51 -0500

    Add NetworkCommand class extending NetworkCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/NetworkCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class NetworkCommand extends NetworkCmd {
+    public NetworkCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
