7ecc0b69a0a97d90b8216d050791e5c984549d6d
YesithTK <yesithszc@gmail.com>
2026-04-24 20:10:15 -0500

    Add MsgCommand class for message handling

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/MsgCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class MsgCommand extends MsgCmd {
+    public MsgCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
