b291a1ff81c962fa13cd830b8323afa9eb877ac3
YesithTK <yesithszc@gmail.com>
2026-04-24 20:06:03 -0500

    Add BroadcastCommand class extending BroadcastCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/BroadcastCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class BroadcastCommand extends BroadcastCmd {
+    public BroadcastCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
