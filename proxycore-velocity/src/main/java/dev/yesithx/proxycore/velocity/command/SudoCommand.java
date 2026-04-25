e7b2a8143cbef34e21c3c9a023b55ebde46f3859
YesithTK <yesithszc@gmail.com>
2026-04-24 20:16:17 -0500

    Add SudoCommand class extending SudoCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/SudoCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class SudoCommand extends SudoCmd {
+    public SudoCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
