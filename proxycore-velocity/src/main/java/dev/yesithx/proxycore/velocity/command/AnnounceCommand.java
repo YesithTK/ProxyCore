3e67fd194c379d36f70ff9eae39b501578b2e434
YesithTK <yesithszc@gmail.com>
2026-04-24 20:05:24 -0500

    Add AnnounceCommand class for announcements

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/AnnounceCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class AnnounceCommand extends AnnounceCmd {
+    public AnnounceCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
