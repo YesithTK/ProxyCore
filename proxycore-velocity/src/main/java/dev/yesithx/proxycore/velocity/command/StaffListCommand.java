c23cc8ffc6f7413b1a4f1d66d1fbf4b4fe16270c
YesithTK <yesithszc@gmail.com>
2026-04-24 20:15:33 -0500

    Add StaffListCommand class for staff listing

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/StaffListCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class StaffListCommand extends StaffListCmd {
+    public StaffListCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
