f4e1fe42d04f10d702314840f515a689ee6e0a37
YesithTK <yesithszc@gmail.com>
2026-04-24 20:08:20 -0500

    Add IPCommand class for handling IP commands

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/IPCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class IPCommand extends IPCmd {
+    public IPCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
