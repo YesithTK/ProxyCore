f060e056a8adc2ee1da03d3a510b4c4ce5cba40c
YesithTK <yesithszc@gmail.com>
2026-04-24 20:09:30 -0500

    Add MaintenanceCommand class extending MaintenanceCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/MaintenanceCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class MaintenanceCommand extends MaintenanceCmd {
+    public MaintenanceCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
