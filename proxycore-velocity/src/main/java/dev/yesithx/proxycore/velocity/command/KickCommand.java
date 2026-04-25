c79c1ce6209ef7e1de78ec756acc1448f6caa0fc
YesithTK <yesithszc@gmail.com>
2026-04-24 20:08:52 -0500

    Add KickCommand class for handling kick commands

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/KickCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class KickCommand extends KickCmd {
+    public KickCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
