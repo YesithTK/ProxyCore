00e5f244f4123e052c709f4d421c44be64e205a9
YesithTK <yesithszc@gmail.com>
2026-04-24 20:04:49 -0500

    Add AlertCommand class extending AlertCmd

---
+++ proxycore-velocity/src/main/java/dev/yesithx/proxycore/velocity/command/AlertCommand.java
@@ -0,0 +1,5 @@
+package dev.yesithx.proxycore.velocity.command;
+import dev.yesithx.proxycore.velocity.ProxyCoreVelocity;
+public final class AlertCommand extends AlertCmd {
+    public AlertCommand(ProxyCoreVelocity plugin) { super(plugin); }
+}
